31d965d647b8ab31a8bea22303e6b73f11758938
JackDownload <kismocsy@gmail.com>
2020-02-09 12:11:03 +0100

    Update BaelGrid.vue

--- components/BaelGrid.vue
+++ components/BaelGrid.vue
@@ -4,6 +4,7 @@
 
       <div v-if="items2[pi]" v-for="(p,pi) in items2" :key="p.pi" class="xs-border xs-p2 bcg-item">
         <div class="item xs-block xs-full-height xs-flex">
+        <div class="feat-wrapper" v-if="theThumb"><transition appear name="fade"><img class="featured-image" :src="thumbnail" :alt="title"></transition></div>
           <nuxt-link class="xs-text-center xs-flex xs-full-height xs-flex-align-center xs-flex-justify-center xs-text-center" :to="p._path">
             {{p.title}}
           </nuxt-link>
